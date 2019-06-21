67c1b37bba2f5eb7870df2ff4489827ebc4d9941
Marius Muntean <marius.muntean@live.com>
2019-06-21 18:07:02 +0200

    initial

---
+++ RainbowLoading/RainbowLoading.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RainbowLoading
+{
+    public partial class Rainbowoading : ContentView
+    {
+        public Rainbowoading()
+        {
+            InitializeComponent();
+        }
+    }
+}
